c3aad6f984cb711b3d49d3d1d5ba2e1e60d0afdf
Maulana <maulanayusupp@gmail.com>
2025-02-01 19:39:31 +0700

    update footer

--- layouts/partials/TheFooter.vue
+++ layouts/partials/TheFooter.vue
@@ -42,7 +42,7 @@
       </div>
 			<div class="mt-8 border-t border-gray-900/10 pt-8 md:flex md:items-center md:justify-between">
         <div class="flex space-x-6 md:order-2">
-          <a v-for="item in navigation.social" :key="item.name" :href="item.href" class="text-gray-400 hover:text-gray-500">
+          <a v-for="item in navigation.social" :key="item.name" :href="item.href" target="_blank" class="text-gray-400 hover:text-gray-500">
             <span class="sr-only">{{ item.name }}</span>
             <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
           </a>
@@ -72,23 +72,23 @@ const navigation = {
     { name: 'Terms', href: '#' },
   ],
   social: [
-    {
-      name: 'Facebook',
-      href: 'https://www.instagram.com/',
-      icon: defineComponent({
-        render: () =>
-          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
-            h('path', {
-              'fill-rule': 'evenodd',
-              d: 'M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z',
-              'clip-rule': 'evenodd',
-            }),
-          ]),
-      }),
-    },
+    // {
+    //   name: 'Facebook',
+    //   href: 'https://www.instagram.com/',
+    //   icon: defineComponent({
+    //     render: () =>
+    //       h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
+    //         h('path', {
+    //           'fill-rule': 'evenodd',
+    //           d: 'M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z',
+    //           'clip-rule': 'evenodd',
+    //         }),
+    //       ]),
+    //   }),
+    // },
     {
       name: 'Instagram',
-      href: 'https://www.instagram.com/',
+      href: 'https://www.instagram.com/temanisikecil',
       icon: defineComponent({
         render: () =>
           h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
@@ -100,20 +100,20 @@ const navigation = {
           ]),
       }),
     },
-    {
-      name: 'YouTube',
-      href: 'https://www.instagram.com/',
-      icon: defineComponent({
-        render: () =>
-          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
-            h('path', {
-              'fill-rule': 'evenodd',
-              d: 'M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z',
-              'clip-rule': 'evenodd',
-            }),
-          ]),
-      }),
-    },
+    // {
+    //   name: 'YouTube',
+    //   href: 'https://www.instagram.com/',
+    //   icon: defineComponent({
+    //     render: () =>
+    //       h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
+    //         h('path', {
+    //           'fill-rule': 'evenodd',
+    //           d: 'M19.812 5.418c.861.23 1.538.907 1.768 1.768C21.998 8.746 22 12 22 12s0 3.255-.418 4.814a2.504 2.504 0 0 1-1.768 1.768c-1.56.419-7.814.419-7.814.419s-6.255 0-7.814-.419a2.505 2.505 0 0 1-1.768-1.768C2 15.255 2 12 2 12s0-3.255.417-4.814a2.507 2.507 0 0 1 1.768-1.768C5.744 5 11.998 5 11.998 5s6.255 0 7.814.418ZM15.194 12 10 15V9l5.194 3Z',
+    //           'clip-rule': 'evenodd',
+    //         }),
+    //       ]),
+    //   }),
+    // },
   ],
 }
 </script>
