8b7727c6f2d2870a3185d90898278b660aaba4ca
Maulana <maulanayusupp@gmail.com>
2025-02-01 19:27:22 +0700

    Update price

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -37,14 +37,14 @@
             <img :src="product.imageSrc" :alt="product.imageAlt" class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-96" />
             <div class="mt-4 flex justify-between">
               <div>
-                <h3 class="text-sm text-gray-700">
+                <h3 class="text-md text-gray-700">
                   <a :href="product.href">
                     <span aria-hidden="true" class="absolute inset-0" />
                     {{ product.name }}
                   </a>
                 </h3>
               </div>
-              <p class="text-sm font-medium text-gray-900">{{ product.price }}</p>
+              <!-- <p class="text-sm font-medium text-gray-900">{{ product.price }}</p> -->
             </div>
           </div>
         </div>
@@ -62,14 +62,14 @@
             <img :src="product.imageSrc" :alt="product.imageAlt" class="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-96" />
             <div class="mt-4 flex justify-between">
               <div>
-                <h3 class="text-sm text-gray-700">
+                <h3 class="text-md text-gray-700">
                   <a :href="product.href">
                     <span aria-hidden="true" class="absolute inset-0" />
                     {{ product.name }}
                   </a>
                 </h3>
               </div>
-              <p class="text-sm font-medium text-gray-900">{{ product.price }}</p>
+              <!-- <p class="text-sm font-medium text-gray-900">{{ product.price }}</p> -->
             </div>
           </div>
         </div>
