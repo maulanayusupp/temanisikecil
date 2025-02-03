03550d1562d8dbd63506c4458be7b4b27d5243b9
Maulana Yusup Abdullah <maulanayusupp@gmail.com>
2025-02-03 15:26:17 +0700

    QUICK FIX AND UPDATE COMMIT: Add/Update fixes components

--- pages/index.vue
+++ pages/index.vue
@@ -3,22 +3,38 @@
 	<main>
 		<!-- Intro -->
 		<div class="relative isolate">
-			<div class="absolute left-1/2 right-0 top-0 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48" aria-hidden="true">
-				<div class="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30" style="clip-path: polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)" />
+			<div class="bg-white">
+				<div class="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+					<div class="relative isolate overflow-hidden bg-[#154847] px-6 py-24 text-center shadow-2xl sm:rounded-3xl sm:px-16">
+						<h2 class="text-balance text-4xl font-semibold tracking-tight text-white sm:text-5xl">Temanisikecil</h2>
+						<p class="mx-auto mt-6 max-w-3xl text-pretty text-lg/8 text-gray-300">
+							<span class="font-bold text-white">Ini lho cara seru biar anak gak cuma nonton terus!</span> Dengan worksheet interaktif dari Temanisikecil, anak bisa belajar sambil bermain, mengasah logika, dan mengembangkan kreativitas dengan cara yang menyenangkan. Yuk, bantu si kecil mengeksplorasi dunia baru tanpa terlalu banyak waktu di depan layar!
+						</p>
+						<div class="mt-10 flex items-center justify-center gap-x-6">
+							<a
+								href="https://lynk.id/temanisikecil"
+								target="_blank"
+								class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
+								Lihat Produk
+							</a>
+						</div>
+						<svg viewBox="0 0 1024 1024" class="absolute left-1/2 top-1/2 -z-10 size-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]" aria-hidden="true">
+							<circle cx="512" cy="512" r="512" fill="url(#827591b1-ce8c-4110-b064-7cb85a0b1217)" fill-opacity="0.7" />
+							<defs>
+								<radialGradient id="827591b1-ce8c-4110-b064-7cb85a0b1217">
+									<stop stop-color="#154847" />
+									<stop offset="1" stop-color="#154847" />
+								</radialGradient>
+							</defs>
+						</svg>
+					</div>
+				</div>
 			</div>
+
 			<div class="overflow-hidden">
 				<div class="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
 					<div class="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
 						<div class="relative w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
-							<h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-								Temanisikecil
-							</h1>
-							<p class="mt-6 text-lg text-gray-600 sm:max-w-md lg:max-w-none">
-								<span class="text-black font-bold">Ini lho cara seru biar anak gak cuma nonton terus!</span>
-								<br>
-								Dengan worksheet interaktif dari Temanisikecil, anak bisa belajar sambil bermain, mengasah logika, dan mengembangkan kreativitas dengan cara yang menyenangkan. Yuk, bantu si kecil mengeksplorasi dunia baru tanpa terlalu banyak waktu di depan layar!
-							</p>
-
 							<div class="max-w-2xl text-gray-800 mt-8">
 								<h2 class="text-lg font-bold mb-4">Seru & Edukatif! Worksheet Aktivitas untuk Anak 🎉</h2>
 
